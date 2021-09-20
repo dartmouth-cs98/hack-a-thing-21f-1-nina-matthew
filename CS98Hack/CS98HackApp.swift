0439cb5a6b33b0fdbcd66bb3687e441b4a7f2ab8
Nina Herman <nina.n.herman.22@dartmouth.edu>
2021-09-20 11:50:12 -0400

    Initial Commit

---
+++ CS98Hack/CS98HackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS98HackApp.swift
+//  CS98Hack
+//
+//  Created by Nina Herman on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct CS98HackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
