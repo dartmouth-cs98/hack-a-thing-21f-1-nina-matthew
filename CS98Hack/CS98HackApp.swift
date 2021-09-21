31ff36e647efbe8205372f35d9554fa0693dcfa1
Matthew C. Roth <matthew.c.roth.22@dartmouth.edu>
2021-09-21 13:08:36 -0400

    add map functionality

--- CS98Hack/CS98HackApp.swift
+++ CS98Hack/CS98HackApp.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 @main
 struct CS98HackApp: App {
+    @StateObject var locations = Locations()
+    
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ContentView(location: Location.example)
+            TabView {
+                NavigationView {
+                    ContentView(location: locations.primary)
+                }
+                .tabItem {
+                    Image(systemName: "airplane.circle.fill")
+                    Text("Discover")
+                }
+                
+                NavigationView {
+                    WorldView()
+                }
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Locations")
+                }
             }
+            .environmentObject(locations)
         }
     }
 }
---
+++ CS98Hack/Locations.swift
@@ -0,0 +1,22 @@
+//
+//  Locations.swift
+//  CS98Hack
+//
+//  Created by Matt Roth on 9/21/21.
+//
+
+import Foundation
+
+class Locations: ObservableObject {
+    let places: [Location]
+    
+    var primary: Location {
+        places[0]
+    }
+    
+    init() {
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        places = try! JSONDecoder().decode([Location].self, from: data)
+    }
+}
